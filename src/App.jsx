e7d807a58bc0b52ab676fbd7b38464ae16588370
Nikhilsingh478 <neelsingh45940s@gmail.com>
2025-01-07 21:15:24 +0530

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./Components/card";
+import Card from "./Components/Card";
 import Card2 from "./Components/Card2";
 function App() {
   const data = ["harsh", "nikhil", "vedant"];
